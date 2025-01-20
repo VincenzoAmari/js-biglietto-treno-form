e9e28f557a447ad670c75ae5ccea64df3d56c96a
VincenzoAmari <vincenzo.am99@gmail.com>
2025-01-20 19:08:11 +0100

    calcola il prezzo biglietto

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-function calcoloPrezzo() {
-  const eta = parseInt(prompt("quanti anni hai?"));
-  const km = parseFloat(prompt("quanti chilometri vuoi percorrere?"));
-  const prezzoBase = km * 0.21;
-  let prezzoFinale = prezzoBase;
+// function calcoloPrezzo() {
+//   const eta = parseInt(prompt("quanti anni hai?"));
+//   const km = parseFloat(prompt("quanti chilometri vuoi percorrere?"));
+//   const prezzoBase = km * 0.21;
+//   let prezzoFinale = prezzoBase;
 
-  if (eta < 18) {
-    prezzoFinale = prezzoBase * 0.8;
-  } else if (eta > 65) {
-    prezzoFinale = prezzoBase * 0.6;
-  }
+//   if (eta < 18) {
+//     prezzoFinale = prezzoBase * 0.8;
+//   } else if (eta > 65) {
+//     prezzoFinale = prezzoBase * 0.6;
+//   }
 
-  alert("per te il costo del biglietto è: €" + prezzoFinale.toFixed(2));
-}
+//   alert("per te il costo del biglietto è: €" + prezzoFinale.toFixed(2));
+// }
 
-calcoloPrezzo();
+// calcoloPrezzo();
