1596af068e046667b3129f52d9d030604fb8a415
VincenzoAmari <vincenzo.am99@gmail.com>
2025-01-20 20:51:26 +0100

    update JS

--- main.js
+++ main.js
@@ -1,16 +1,23 @@
-// function calcoloPrezzo() {
-//   const eta = parseInt(prompt("quanti anni hai?"));
-//   const km = parseFloat(prompt("quanti chilometri vuoi percorrere?"));
-//   const prezzoBase = km * 0.21;
-//   let prezzoFinale = prezzoBase;
+document.getElementById("genera").addEventListener("click", function () {
+  const nome = document.getElementById("nome").value;
+  const km = parseFloat(document.getElementById("km").value);
+  const eta = document.getElementById("eta").value;
 
-//   if (eta < 18) {
-//     prezzoFinale = prezzoBase * 0.8;
-//   } else if (eta > 65) {
-//     prezzoFinale = prezzoBase * 0.6;
-//   }
+  const prezzoBase = km * 0.21;
+  let prezzoFinale = prezzoBase;
+  let offerta = "Biglietto Standard";
 
-//   alert("per te il costo del biglietto è: €" + prezzoFinale.toFixed(2));
-// }
+  if (!nome || isNaN(km) || km <= 0) {
+    alert("Per favore, inserisci tutti i dati correttamente.");
+    return;
+  }
+  //   if (eta < 18) {
+  //     prezzoFinale = prezzoBase * 0.8;
+  //   } else if (eta > 65) {
+  //     prezzoFinale = prezzoBase * 0.6;
+  //   }
+
+  //   alert("per te il costo del biglietto è: €" + prezzoFinale.toFixed(2));
+});
 
 // calcoloPrezzo();
